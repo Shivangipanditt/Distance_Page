2aa5d915fd2fdd5a6a2692074ce7e735752dfb1c
shivuu0212 <shivangisharma2122000@gmail.com>
2025-09-28 17:35:39 +0530

    ww

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -3,10 +3,14 @@ export default function Navbar() {
   <>
   <div className="flex justify-between p-4 ml-30 mr-30 border-b-1  border-orange-300">
 
-    <div className="text-5xl text-blue-500 mt-2">
-logo
-</div>
-
+     {/* Logo Section */}
+        <div className="flex items-center">
+          <img 
+            src="/edge-logo.webp" 
+            alt="Logo" 
+            className="h-15 w-auto object-contain" 
+          />
+        </div>
 <div className="text-[15px] font-bold text-gray-700 mt-5">
 <ul className="cursor-pointer">
     <li className="inline p-4">Home  <span className="  ml-4 text-orange-300"> |</span></li>
